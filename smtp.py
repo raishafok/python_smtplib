ad1f165236f6d750e58444f1b57bf79a5c17aef0
Raishaf OK <raishafok@gmail.com>
2018-11-13 20:30:13 +0530

    Add files via upload

---
+++ smtp.py
@@ -0,0 +1,22 @@
+
+# Python code to illustrate Sending mail from  
+# your Gmail account  
+import smtplib 
+  
+# creates SMTP session 
+s = smtplib.SMTP('smtp.gmail.com', 587) 
+  
+# start TLS for security 
+s.starttls() 
+  
+# Authentication 
+s.login("sender_mail", "password") 
+  
+# message to be sent 
+message = "Message_you_need_to_send"
+  
+# sending the mail 
+s.sendmail("sender mail", "reciever mail", message) 
+  
+# terminating the session 
+s.quit()
